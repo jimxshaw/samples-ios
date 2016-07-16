7d75dc12524b5e1c449ed984d4a43fb1ec929512
Jim Shaw <jimxshaw@gmail.com>
2016-07-16 11:24:25 -0500

    added Personal Security Detail project boilerplate

---
+++ Personal Security Detail/Personal Security Detail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personal Security Detail
+//
+//  Created by James Shaw on 7/16/16.
+//  Copyright © 2016 Jimmy Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
